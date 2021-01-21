376e939ed8e5ed65a57d8c17093c66ad6614d244
SuhDongHyun <adam008@khu.ac.kr>
2021-01-22 01:00:47 +0900

    Update my code

--- baseball_game.py
+++ baseball_game.py
@@ -270,42 +270,45 @@ def is_no(one_more_input):
 
 
 def main():
+    # start game
     print("Play Baseball")
-    #user_input = 999
-    while True:
-        random_number = str(get_not_duplicated_three_digit_number())
-        print("Random Number is : ", random_number)
-        # ===Modify codes below=============
-        # 위의 코드를 포함하여 자유로운 수정이 가능함
-        while True:
-            user_input_number = input('Input guess number : ')
-            if is_validated_number(user_input_number):
-                result = get_strikes_or_ball(user_input_number, random_number)
-                if result == [3, 0]:
-                    print('You win, one more(Y/N) ?')
-                    again = False
-                    while True:
-                        play_again = input()
-                        if is_yes(play_again):
-                            again = True
-                            break
-                        elif is_no(play_again):
-                            break
-                        else:
-                            print('Wrong Input, Input again')
-                            continue
-                    if again:
-                        continue
-                    else:
+    random_number = str(get_not_duplicated_three_digit_number())
+    print("Random Number is : ", random_number)
+
+    stop = False
+    user_input = input('Input guess number : ')
+
+    while not stop:
+        if is_validated_number(user_input):
+            res = get_strikes_or_ball(user_input,random_number)
+            print(f"Strikes:{res[0]}, Balls:{res[1]}")
+            if res == [3,0]:
+                # 3 strikes
+                user_input = input('You win, one more(Y/N)?')
+                while True:
+                    if is_yes(user_input):
+                        random_number = str(get_not_duplicated_three_digit_number())
+                        print("Random Number is : ", random_number)
+                        user_input = input('Input guess number : ')
                         break
-                else:
-                    print('Strikes :', result[0], ', Balls :', result[1])
+                    elif is_no(user_input):
+                        stop = True
+                        break
+                    else:
+                        print('Wrong Input, Input again')
+                        user_input = input('Input guess number : ')
             else:
-                print('Wrong Input, Input again')
-
-
-
-    # ==================================
+                # re-try
+                user_input = input('Input guess number : ')
+        elif user_input == '0':
+            # edge case
+            break
+        else:
+            # wrong input
+            print('Wrong Input, Input again')
+            user_input = input('Input guess number : ')
+            
+    # end game
     print("Thank you for using this program")
     print("End of the Game")
 
